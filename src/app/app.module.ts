cd18f8dae9e808250c16a661cca13603e6c3aaf5
Thomas Cober <tcober5@gmail.com>
2018-01-01 20:42:55 -0600

    Setting up posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TomReviewsRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TomReviewsRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
